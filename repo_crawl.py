d75d59aa53c09361103e2f4b329b3c3fedd98ce6
Jeff Lupker <jeffreylupker@gmail.com>
2020-09-18 11:51:23 -0400

    Update repo_crawl.py

--- repo_crawl.py
+++ repo_crawl.py
@@ -3,57 +3,59 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
 def repo_crawl(input_url, output_dir):
-  base_url = input_url
-  page = requests.get(base_url)
-  soup = BeautifulSoup(page.content, 'html.parser')
+    base_url = input_url
+    page = requests.get(base_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-  """
-  Crawl through directories looking for .py files
-  """
+    """
+    Crawl through directories looking for .py files
+    """
 
-  print('Crawling Github for .py files...')
-  for span in soup.find_all('a', class_="js-navigation-open link-gray-dark"):
-      if '.py' in span.text:
-          url = base_url + '/blob/master/' + span.text
+    print('Crawling Github for .py files...')
+    for span in soup.find_all('a', class_="js-navigation-open link-gray-dark"):
+        if '.py' in span.text:
+            url = base_url + '/blob/master/' + span.text
 
-          page = requests.get(url)
-          soup = BeautifulSoup(page.content, 'html.parser')
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
 
-          corpus = ''
-          for td in soup.find_all('td'):
-              corpus = corpus + td.text + '\n'
+            corpus = ''
+            for td in soup.find_all('td'):
+                corpus = corpus + td.text + '\n'
 
-          corpus = corpus.replace('\n\n', '\n')
+            corpus = corpus.replace('\n\n', '\n')
 
-          with open('output_dir, 'a+') as f:
-              f.write(corpus)
+            with open(output_dir, 'a+') as f:
+                f.write(corpus)
 
-      else:
-          try:
-              dir_link = span.text
-              span_url = base_url + '/tree/master/' + dir_link
-              page = requests.get(span_url)
-              soup = BeautifulSoup(page.content, 'html.parser')
+        else:
+            try:
+                dir_link = span.text
+                span_url = base_url + '/tree/master/' + dir_link
+                page = requests.get(span_url)
+                soup = BeautifulSoup(page.content, 'html.parser')
 
-              for span in soup.find_all('a', class_="js-navigation-open link-gray-dark"):
-                  if '.py' in span.text:
-                      py_url = base_url + '/blob/master/' + dir_link + '/' + span.text
-                      print(py_url)
-                      page = requests.get(py_url)
-                      soup = BeautifulSoup(page.content, 'html.parser')
+                for span in soup.find_all('a', class_="js-navigation-open link-gray-dark"):
+                    if '.py' in span.text:
+                        py_url = base_url + '/blob/master/' + dir_link + '/' + span.text
+                        print(py_url)
+                        page = requests.get(py_url)
+                        soup = BeautifulSoup(page.content, 'html.parser')
 
-                      corpus = ''
-                      for td in soup.find_all('td'):
-                          corpus = corpus + td.text + '\n'
+                        corpus = ''
+                        for td in soup.find_all('td'):
+                            corpus = corpus + td.text + '\n'
 
-                      corpus = corpus.replace('\n\n', '\n')
+                        corpus = corpus.replace('\n\n', '\n')
 
-                      with open(output_dir,'a+') as f:
-                          f.write(corpus)
-          except:
-              pass
-  print('Done!')
+                        with open(output_dir, 'a+') as f:
+                            f.write(corpus)
+            except:
+                pass
+    print('Done!')
+
 
 if __name__ == "__main__":
     repo_crawl(sys.argv[0], sys.argv[1])
